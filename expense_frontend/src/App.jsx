66bcb60ba5cae9d54cfdc8c50279fcf6f069661f
philipjohnson0229 <philip.johnson0229@gmail.com>
2022-07-17 22:51:56 -0400

    Working on walking skeleton of front end in react

---
+++ expense_frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { ClassComponent } from "./components/ClassComponent";
+
+export const App = () =>
+{
+    return (
+        <>
+        <h1>Hello</h1>
+        <h2>Hello Again</h2>
+        <ClassComponent/>
+        </>
+    );
+}
+
+export default App;
---
+++ expense_frontend/src/components/ClassComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export class ClassComponent extends React.Component
+{
+    constructor()
+    {
+        super();
+    }
+
+    render()
+    {
+        return(
+            <>
+            <h1>hello there this is the class component</h1>
+            </>
+        );
+    }
+}
